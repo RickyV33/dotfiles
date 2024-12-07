623662734c13e0961d11c86ba1e2c1c876bd85f2
Ricky Valencia <3d.rickyv@gmail.com>
2024-12-07 09:27:25 -0800

    remove brew flag

--- brew.sh
+++ brew.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # install brew
-if [ "$WITH_BREW" = true ]; then
+# if [ "$WITH_BREW" = true ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     sudo apt-get install build-essential
-fi
+# fi
 
 brew install gcc
 
