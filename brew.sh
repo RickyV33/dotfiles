7df2f85b211964225da1593f5d9639add2aaf645
Ricky Valencia <3d.rickyv@gmail.com>
2024-12-08 22:33:41 -0800

    add brew flag

--- brew.sh
+++ brew.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # install brew
-# if [ "$WITH_BREW" = true ]; then
+if [ "$WITH_BREW" = true ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     sudo apt-get install build-essential
-# fi
 
-brew install gcc
+    brew install gcc
 
-# install brew packages
-brew install scc tldr thefuck duf eza bat
-echo 'eval $(thefuck --alias)' >> ~/.zshrc
+    # install brew packages
+    brew install scc tldr thefuck duf eza bat
+    echo 'eval $(thefuck --alias)' >> ~/.zshrc
+fi
+
